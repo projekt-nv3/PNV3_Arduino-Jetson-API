d819de1a6abddb8867bb8645d4bced9fb7a1eecf
Elevenv <vitthaljadhav1420@gmail.com>
2023-06-05 04:17:16 +0530

    view location feature added

--- read_ard_data.py
+++ read_ard_data.py
@@ -1,6 +1,11 @@
 import serial
 import time
 import sys
+from IPython.display import display, HTML
+
+latitude = 18.5204303 # Example latitude
+longitude = 73.8567437  # Example longitude
+
 # ["b'-11111", '3216', '-864', '17056', '4240', '368', '235', '290.48', '233', "'"]
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
@@ -11,6 +16,14 @@ while True:
             data = [str(i) for i in str(data).split('|') if not i=="'"]
             # print(data)
             print("Location: ",data[0][2:])
+            if data[0][2:] != "-11111":
+                location = data[0][2:]
+                l = location.split(',')
+                latitude = l[0]
+                longitude = l[1]
+                button_html = f'<button onclick="window.open(\'https://www.google.com/maps/search/?api=1&query={latitude},{longitude}\', \'_blank\')">Go to Location</button>'
+                display(HTML(button_html))
+
             print("AX: ",data[1])
             print("AY: ",data[2])
             print("AZ: ",data[3])
